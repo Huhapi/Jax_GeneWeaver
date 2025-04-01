b38c2788e24ca0cf1ce6d6d809b2ea8c5de68a2d
Huhapi <157430120+Huhapi@users.noreply.github.com>
2025-04-01 13:58:04 -0400

    Adusting MSET test files

--- tests/unit/MSET_tests/MSET_test.py
+++ tests/unit/MSET_tests/MSET_test.py
@@ -6,9 +6,9 @@ import os
 
 if __name__ == "__main__":
     # Define your file paths.
-    file_path_1 = os.path.abspath("../Jax_GeneWeaver/src/tests/unit/RATUS1")
-    file_path_2 = os.path.abspath("../Jax_GeneWeaver/src/tests/unit/ratus2")
-    background_file_path = os.path.abspath("../Jax_GeneWeaver/src/tests/unit/KEGGRattusnorvegicusBG.txt")
+    file_path_1 = os.path.abspath("../Jax_GeneWeaver/src/tests/unit/MSET_tests/RATUS1")
+    file_path_2 = os.path.abspath("../Jax_GeneWeaver/src/tests/unit/MSET_tests/ratus2")
+    background_file_path = os.path.abspath("../Jax_GeneWeaver/src/tests/unit/MSET_tests/KEGGRattusnorvegicusBG.txt")
     
     # Prepare the input data. Notice that we include the background_file_path.
     input_data = {
