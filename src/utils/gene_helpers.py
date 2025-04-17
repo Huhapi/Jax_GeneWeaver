4d79e47475e1ed62ecd0b2ce510efb0df0a38d18
Huhapi <157430120+Huhapi@users.noreply.github.com>
2025-04-17 00:03:13 -0400

    util and gene helper tests

--- src/utils/gene_helpers.py
+++ src/utils/gene_helpers.py
@@ -2,8 +2,13 @@ from typing import List
 
 
 def extract_genes_from_gw(file_content: str) -> List[str]:
+    """
+    This extracts genes from the contents of a gene set file.
+    It assumes the first 16 lines are unuseful data for genes.
+    It also avoids the line starting with the characters below.
+    """
     genes = []
-    skip_chars = ("#", ":", "=", "+", "@", "%", "!", "Q")
+    skip_chars = ("#", ":", "=", "+", "@", "%", "!")
     count = 0
     for line in file_content.splitlines():
         count+= 1
@@ -11,14 +16,26 @@ def extract_genes_from_gw(file_content: str) -> List[str]:
             line = line.strip()
             if not line or line.startswith(skip_chars):
                 continue
-            gene = line.split()[0]
+            line_split = line.split()
+            gene = line_split[0]
+            if(len(line_split)>2):
+                gene += " "+line_split[1]
             genes.append(gene)
     return genes
 
 def extract_bg_genes(file_content: str) -> List[str]:
+    """
+    Extracts genes from the content of a background gene file which only has genes
+    in it.
+    """
     genes = []
     for line in file_content.splitlines():
         line = line.strip()
-        gene = line.split()[0]
-        genes.append(gene)
+        split_line = line.split()
+        if(len(split_line)>0):
+            gene = split_line[0]
+            if(len(split_line)>1):
+                gene += " "+split_line[1]
+            genes.append(gene)
+            
     return genes
--- tests/unit/utils_tests/utils_tests.py
+++ tests/unit/utils_tests/utils_tests.py
@@ -1,6 +1,80 @@
 # Test section for utils, accessing genes from GeneWeaver ReST API
 from plugins.api.geneSetRestAPI import fetchGeneSymbols_from_geneset
+from utils.gene_helpers import extract_genes_from_gw, extract_bg_genes
+import os
 
-if __name__=="__main__":
+def test_extract_genes_from_gw():
+    """ Testing utils.gene_helpers.extract_genes_from_gw """
+    current_dir = os.path.dirname(__file__)
+    file_path_1 = os.path.join(current_dir, "ptest_ratus_1")
+    with open(file_path_1, "r") as f:
+        content1 = f.read()
+    genes = extract_genes_from_gw(content1)
+    list = [ "Akt1", "Akt",	"Akt2",	"Bax", "Bcl2", "Bcl-2", "Bcl2l1", "Bcl-xl", "Bcl2l", "Bclx", "bcl-X", "Capn1", "CANP 1", "muCANP", "Capn2", "Casp3"]
+
+    passed = True
+    for x in range(len(genes)):
+        if(not list[x] == genes[x]):
+            passed = False
+            break
+    if(passed):
+        print("Extract genes from gw success.")
+    else:
+        print("Extract genes from gw failed.")
+
+
+
+def test_extract_bg_genes():
+    """ Testing utils.gene_helpers.extract_bg_genes """
+    current_dir = os.path.dirname(__file__)
+    file_path_1 = os.path.join(current_dir, "ptest_ratus_bg")
+    with open(file_path_1, "r") as f:
+        content1 = f.read()
+    genes = extract_bg_genes(content1)
+    genelist = [
+        "Akt1",	
+        "Akt",	
+        "Akt2",	
+        "Bax",	
+        "Bcl2",	 
+        "Bcl-2",	
+        "Bcl2l1",	
+        "Bcl-xl",	
+        "Bcl2l",	
+        "Bclx",	
+        "bcl-X",	
+        "Capn1",	
+        "CANP 1",	
+        "muCANP",	
+        "Capn2",	
+        "Casp3",	
+        "CPP32-beta",	
+        "Lice",	
+        "Yama",	
+        "Cycs",	
+        "CYCSA",	
+        "Cyct",	
+        "CYCTA",	
+        "Il1a",	
+        "IL-1 alpha",	
+        "IL-1F1"
+    ]
+    passed = True
+    for x in range(len(genes)):
+        if(not genelist[x] == genes[x]):
+            passed = False
+            break
+    if(passed):
+        print("Extract bg genes success.")
+    else:
+        print("Extract bg genes failed.")
+    
+
+def test_fetchGeneSymbols():
     symbols=fetchGeneSymbols_from_geneset(233325) # https://www.geneweaver.org/viewgenesetdetails/219249
     print("Gene Symbols: ",((symbols)))
+
+
+if __name__=="__main__":
+    test_extract_genes_from_gw()
+    test_extract_bg_genes()
